9aee0f95b7f17ab14182f29ddc66dff1d4621d0a
MarioRaudales23 <marioraudales33@unitec.edu>
2017-09-24 19:54:15 -0600

    metodo mail

--- src/proyecto_tdb/principal.java
+++ src/proyecto_tdb/principal.java
@@ -10,8 +10,16 @@ import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
@@ -2729,7 +2737,42 @@ public class principal extends javax.swing.JFrame {
         this.jd_registroDeCitas.setResizable(false);
         this.jd_registroDeCitas.setVisible(true);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
+    
+    boolean enviarmail(String mail){
+        final String username = "milton.pasos@gmail.com";
+		final String password = "cerrar12345";
 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("milton.pasos@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse(mail));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler,"
+				+ "\n\n No spam to my email, please!");
+
+			Transport.send(message);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+    }
     /**
      * @param args the command line arguments
      */
